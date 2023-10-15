01b265e77f74131272f181ccb8978074bee242fa
unknown <tirth4ur@gmail.com>
2023-10-15 13:12:52 -0400

    added timer and difficulty functionality

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const resultDisplay = document.getElementById('result-display');
 const windowTitle = document.getElementById('window-title');
 const correctCount = document.getElementById('correct-count');
 const incorrectCount = document.getElementById('incorrect-count');
+
+const timeBar = document.getElementById('time-bar');
+let timer;
+
 let history = ['', '', ''];
 const characters = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐔", "🐧", "🦆", "🦉", "🦇", "🦅", "🦋"];
 let speed = 0;
@@ -50,7 +54,8 @@ function checkMatch(userSaidYes) {
 
     correctCount.textContent = "Correct: " + record.correct;
     incorrectCount.textContent = "Incorrect: " + record.incorrect;
-    
+    clearInterval(timer);
+    startTimer();
     nextCharacter();
 }
 
@@ -64,34 +69,62 @@ function nextCharacter() {
 
 nextCharacter(); // Start the game with the first character
 
+
+//close instruction pop up
 function closeInstructions() {
     var modal = document.getElementById("instructions");
     modal.style.display = "none";
 }
+
+
+//decreases timer
+function decreaseTime() {
+    const currentWidth = parseFloat(timeBar.style.width);
+    if (currentWidth > 0) {
+        timeBar.style.width = (currentWidth - 0.1) + '%'; // Decrease by 0.1% per interval. Makes it smoother
+    } 
+    else {
+        //checkMatch will clear current timer and start new one
+        checkMatch(); //add to incorrect when timer runs out and move on 
+    
+    }
+  }
+
+  //starts timer 
+  function startTimer() {
+  
+    // Reset the time bar
+    timeBar.style.width = '100%';
+  
+    timer = setInterval(decreaseTime, speed); //will adjust interval every [speed] seconds
+  }
+
 
 // handles difficulty form submission
 document.getElementById("difficulty").addEventListener("submit", function(event) {
     
     event.preventDefault();
 
-    // Get the selected difficulty level
+
     var selectedDifficulty = document.querySelector('input[name="difficulty"]:checked');
 
     if (selectedDifficulty) {
-        // A difficulty level has been selected
+        // Close window and record difficulty lvl
         closeInstructions();
 
         if(selectedDifficulty.value === "Easy") {
-            speed = 10; 
+            speed = 15; //timer will be 15 seconds
         }
         else if(selectedDifficulty.value === "Medium") {
-            speed = 5; 
+            speed = 7; //7 seconds
         }
         else if(selectedDifficulty.value === "Hard") {
-            speed = 3; 
+            speed = 3; //3 seconds
         }
-        // You can now use 'selectedDifficulty.value' to perform actions based on the user's choice.
-        // For demonstration purposes, we'll just log the selected difficulty.
-        console.log("Speed: " + speed);
+        
+        //starts timer 
+        startTimer();
     }
 });
+
+
